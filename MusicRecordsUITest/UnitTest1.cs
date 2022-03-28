b3432ce96342e9fbcb57e15f5b31c11a475ba322
Kevin Richter Krogsgaard Andersen <88718302+Keveren1@users.noreply.github.com>
2022-03-28 13:55:21 +0200

    Kevin og Malthe UITest med Selenium

--- MusicRecordsUITest/UnitTest1.cs
+++ MusicRecordsUITest/UnitTest1.cs
@@ -8,33 +8,44 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 using Assert = NUnit.Framework.Assert;
+using TestContext = Microsoft.VisualStudio.TestTools.UnitTesting.TestContext;
 
-namespace MusicRecordsUITest;
-[TestClass]
-public class Tests
+namespace MusicRecordsUITest
 {
-    private static readonly string DriverDirectory = "/Users/malthethomsen/Downloads";
-    private static IWebDriver _driver;
-
-    [ClassInitialize]
-    public static void Setup()
+    [TestClass]
+    public class Tests
     {
-        _driver = new ChromeDriver(DriverDirectory); 
+        private static readonly string DriverDirectory = "C:\\WebDrivers";
+        private static IWebDriver _driver;
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            _driver = new ChromeDriver(DriverDirectory);
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            _driver.Dispose();
+        }
+
+        [TestMethod]
+        public void TestMethod()
+        {
+            _driver.Navigate().GoToUrl("http://127.0.0.1:5500/index.html");
+            string title = _driver.Title;
+            Assert.AreEqual("Axios", title);
+
+            IWebElement buttonElement = _driver.FindElement(By.Id("AddData"));
+            buttonElement.Click();
+
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            IWebElement musicList = wait.Until(p => p.FindElement(By.Id("Record list")));
+            Assert.IsTrue(musicList.Text.Contains("Kim Larsen"));
+
+
+        }
     }
 
-    [ClassCleanup]
-    public static void TearDown()
-    {
-        _driver.Dispose();
-    }
-
-    [TestMethod]
-    public void TestMethod()
-    {
-        _driver.Navigate().GoToUrl("http://127.0.0.1:5500/index.html");
-        string title = _driver.Title; 
-        Assert.AreEqual("Axios", title);
-    }
-    
-    
 }
